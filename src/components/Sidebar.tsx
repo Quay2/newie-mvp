6c054e610fd1263a6e22d4be68561f9650f3df41
Jayden Quaynor <JaydenQuaynor@gmail.com>
2024-03-04 14:50:25 -0500

    master

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,29 +1,46 @@
 import React from "react";
-import Image from 'next/image'; // Import the Image component from next/image
-import logo from "C:/Users/JQuay/newie-mvp/public/Logo.svg"; // Import the logo file
+import Image from 'next/image';
+import logo from "/public/Logo.svg";
+import Dashboard from '/public/Dashboard.svg';
+import Community from '/public/Community.svg';
+import Courses from '/public/Courses.svg';
+import Games from '/public/Games.svg';
+import Messages from '/public/Messages.svg';
+import MyClass from '/public/myClass.svg';
+import Settings from '/public/Settings.svg';
+import Profile from '/public/profile.svg';
+import vercel from '/public/vercel.svg';
+
+
+
 const Sidebar = () => {
   return (
-    <aside className="sidebar" style={{backgroundColor: 'black', color: 'white'}}>
-      <div className="flex flex-col items-center justify-between h-full">
-        <div className="sidebar-logo mb-8 flex-1 flex items-center justify-center">
-          <Image className="p-7" src={logo} alt="Logo" /> {/* Use the imported logo with the Image component */}
-        </div>
-        <nav className="sidebar-nav flex-1 flex justify-center items-center">
-          <ul className="flex flex-col items-center justify-center h-full">
-            {/* List items with icons and links flexed */}
-            <li className="sidebar-nav_element p-6 flex justify-center"><a href="#" className="flex gap-4"><Image src="logo" className="icon"/> Item 1</a></li>
-            <li className="sidebar-nav_element p-6 flex justify-center"><a href="#" className="flex gap-4"><Image src="logo" className="icon"/> Item 2</a></li>
-            <li className="sidebar-nav_element p-6 flex justify-center"><a href="#" className="flex gap-4"><Image src="logo" className="icon"/> Item 3</a></li>
-            <li className="sidebar-nav_element p-6 flex justify-center"><a href="#" className="flex gap-4"><Image src="logo" className="icon"/> Item 4</a></li>
-            <li className="sidebar-nav_element p-6 flex justify-center"><a href="#" className="flex gap-4"><Image src="logo" className="icon"/> Item 5</a></li>
-            <li className="sidebar-nav_element p-6 flex justify-center"><a href="#" className="flex gap-4"><Image src="logo" className="icon"/> Item 6</a></li>
-            <li className="sidebar-nav_element p-6 flex justify-center"><a href="#" className="flex gap-4"><Image src="logo" className="icon"/> Item 7</a></li>
+    <aside className="w-[22.75em] h-screen flex flex-col" style={{backgroundColor: 'black', color: 'white'}}>
+
+      <div className="flex flex-col justify-between mx-[2.5em] my-[6.25em] h-full">
+        
+        <div className="flex items-center justify-center w-full h-20">
+          <Image src={logo} alt="Logo" height={50}/> {/* Logo centered within the sidebar */}
+               </div>
+
+        <nav className="sidebar-nav">
+          <ul className="flex flex-col justify-center h-full">
+            <li className="p-6 flex sidebar-nav_element"><a href="#" className="flex gap-4"><Image src={Dashboard} alt="icon"/>Dashboard</a></li>
+            <li className="p-6 flex sidebar-nav_element"><a href="#" className="flex gap-4"><Image src={Community} alt="icon"/>Community</a></li>
+            <li className="p-6 flex sidebar-nav_element"><a href="#" className="flex gap-4"><Image src={Courses} alt="icon"/>Courses</a></li>
+            <li className="p-6 flex sidebar-nav_element"><a href="#" className="flex gap-4"><Image src={Games} alt="icon"/>Games</a></li>
+            <li className="p-6 flex sidebar-nav_element"><a href="#" className="flex gap-4"><Image src={Messages} alt="icon"/>Messages</a></li>
+            <li className="p-6 flex sidebar-nav_element"><a href="#" className="flex gap-4"><Image src={MyClass} alt="icon"/>MyClass</a></li>
+            <li className="p-6 flex sidebar-nav_element"><a href="#" className="flex gap-4"><Image src={Settings} alt="icon"/>Settings</a></li>
           </ul>
         </nav>
-        <div className="sidebar-profile flex-1 flex items-center justify-center my-6 gap-4">
-          <i className="icon">←</i>
-          <span>Profile</span>
-          <i className="icon">→</i>
+
+
+
+        <div className="flex items-center justify-center gap-5">
+          <Image src={Profile} alt="icon"/>
+          <span>Aokiy Ayo</span>
+          <Image src={Settings} alt="icon"/>
         </div>
       </div>
     </aside>
